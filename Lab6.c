b26260d296f76473325f56f955a3960006e6ffee
Clown Warudo <37649552+vaner29@users.noreply.github.com>
2021-07-07 14:48:18 +0300

    Create Lab6.c

---
+++ Lab6.c
@@ -0,0 +1,30 @@
+//var 16
+#include <stdio.h>
+#include <malloc.h>
+
+#define size 4
+
+int main(){
+    double array[size];
+    double* pointerArray = array;
+    array[0] = -3.4;
+    array[1] = 4.5;
+    array[2] = -5.6;
+    array[3] = 6.7;
+    printf("Array using a pointer: ");
+    for (int i = 0; i < size; i++){
+        printf("%g ", *(pointerArray + i));
+    }
+    printf("\nA dynamic Array: ");
+
+
+    double* dynamicArray = (double*) malloc (size * sizeof(double));
+    dynamicArray[0] = -3.4;
+    dynamicArray[1] = 4.5;
+    dynamicArray[2] = -5.6;
+    dynamicArray[3] = 6.7;
+    for (int i = 0; i < size; i++){
+        printf("%g ", dynamicArray[i]);
+    }
+    free(dynamicArray);
+}
