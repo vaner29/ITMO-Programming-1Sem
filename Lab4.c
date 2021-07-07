59f498417681f31428a778017d3939974b2e1730
Clown Warudo <37649552+vaner29@users.noreply.github.com>
2021-07-07 14:47:42 +0300

    Create Lab4.c

---
+++ Lab4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int lowerThan, higherThan, x;
+    scanf("%d", &x);
+    lowerThan = x < 0;
+    higherThan = x > -5;
+    printf("Does X belong to the interval? %d\n", lowerThan&higherThan);
+    scanf("%x", &x);
+    printf ("The 15th bit in X = %x equals %d", x, (x>>14)%2);
+}
